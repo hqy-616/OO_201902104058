6dca909e1259f2ad717e5bf9f4e601f08e58fe17
dell <1332744397@qq.com>
2020-06-08 01:16:34 +0800

    提交了刀片类

--- src/BladeSwitch.java
+++ src/BladeSwitch.java
@@ -2,6 +2,7 @@
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * 描述两个把刀片，相向运动，背向分离，周而复始。两个刀片重叠任何物体，即为攻击，伤害值为1000。
@@ -21,9 +22,9 @@ public class BladeSwitch {
         this.w = w;
         this.h = h;
         //上刀刃的左上角坐标和bladeSwitch相同，w也相同，但高度为一半
-        Blade upBlade = new Blade(Movable.UP, this.x, this.y, this.w, this.h / 2);
+        Blade upBlade = new Blade(Movable.UP, this.x, this.y, this.w, this.h / 2,"imgs/Blade_Up.png");
         //下刀刃的左上角坐标x，y坐标为bladeSwitch竖向中点，w相同，高度为一半
-        Blade downBlade = new Blade(Movable.DOWN, this.x, this.y + this.h / 2, this.w, this.h / 2);
+        Blade downBlade = new Blade(Movable.DOWN, this.x, this.y + this.h / 2, this.w, this.h / 2,"imgs/Blade_Down.png");
         Commons.executorService.execute(upBlade);
         Commons.executorService.execute(downBlade);
     }
@@ -32,6 +33,7 @@ public class BladeSwitch {
      * 运动的刀片
      */
     private class Blade implements CanAttack, OverlapSensitive, Runnable,Shape{
+        //刀片位置
         private int attackDirection = Movable.DOWN;
         private int strength = 100;
         //x,y为左上角坐标
@@ -40,6 +42,8 @@ public class BladeSwitch {
         //w，h为宽度和高度
         private int h;
         private int w;
+        //图片名
+        private String name;
         //自身存在与哪些集合中
         private Collection<Collection> collectionCollection;
 
@@ -55,23 +59,65 @@ public class BladeSwitch {
             );
         }
 
-        public Blade(int attackDirection, int x, int y, int h, int w) {
+        public Blade(int attackDirection, int x, int y, int w, int h,String name) {
             this.attackDirection = attackDirection;
             this.x = x;
             this.y = y;
             this.h = h;
             this.w = w;
+            this.name = name;
         }
 
         @Override
         public void run() {
             //周而复始地运动
-            //如果运动方向是Movable.DOWN，则向下越来越大
-			while(true){
-				this.moveUp();
-				this.moveDown();
-				Helper.sleep(50);
-			}
+            //相背运动缩小，相对运动增大
+            //如果是下刀片，则先向下移再向上移
+            if (this.attackDirection == Movable.DOWN) {
+                while (true) {
+                    for (int i = 0 ; i < 5 ; i++){
+                        //下刀片下移
+                        this.y +=10;
+                        //缩小
+                        this.x += 5;
+                        this.w -= 5;
+                        this.checkOverlap();
+                        Helper.delay(100);
+                    }
+                    for (int i = 0 ; i < 5 ; i++){
+                        //下刀片上移
+                        this.y -= 10;
+                        //放大
+                        this.x -= 5;
+                        this.w += 5;
+                        this.checkOverlap();
+                        Helper.delay(100);
+                    }
+                }
+            }
+            //如果是上刀片，则先向上移再向下移
+            if (this.attackDirection == Movable.UP) {
+                while (true) {
+                    for (int i = 0 ; i < 5 ; i++){
+                        //上刀片上移
+                        this.y -= 10;
+                        //缩小
+                        this.x += 5;
+                        this.w -= 5;
+                        this.checkOverlap();
+                        Helper.delay(100);
+                    }
+                    for (int i = 0 ; i < 5 ; i++){
+                        //上刀片下移
+                        this.y += 10;
+                        //放大
+                        this.x -= 5;
+                        this.w += 5;
+                        this.checkOverlap();
+                        Helper.delay(100);
+                    }
+                }
+            }
         }
 
         //返回中心点横坐标
@@ -110,38 +156,30 @@ public class BladeSwitch {
         @Override
         public void drawMyself(Graphics g) {
             g.drawRect(this.x, this.y, this.w, this.h);
+            g.drawImage(ImgHelper.getImage(this.name),this.x,this.y,this.w,this.h,null);
             Commons.drawingPanel.repaint();
         }
-		//刀片线上移动
-		private void moveUp(){
-            if(this.attackDirection == Movable.UP) {
-                for (int i = 0; i <= 30; i++) {
-                    this.y -= 5;
-                    Helper.delay(100);
-                }
-                this.attackDirection = Movable.DOWN;
-            }
-		}
-        //刀片向下移动
-        private void moveDown(){
-            if(this.attackDirection == Movable.DOWN){
-                for(int i = 0 ; i <= 30 ; i++){
-                    this.y += 5;
-                    Helper.delay(100);
+        //检测重叠
+        private void checkOverlap(){
+            for (OverlapSensitive overlapSensitive: Commons.overlapSensitiveSet) {
+                //判断是否与其他对象重叠
+                if(OverlapUtil.checkOverlap(this,overlapSensitive) && overlapSensitive != this){
+                    //判断重叠的对象是否在canBeAttackedSet集合里
+                    if(Commons.canBeAttackedSet.contains(overlapSensitive)){
+                        ((CanBeAttacked)overlapSensitive).attacked(this);
+                    }
                 }
             }
-
-            this.attackDirection = Movable.UP;
         }
-		
+
     }
     public static void main(String[] args) {
-        Tank tank = new Tank(400,30,50,50,30);
+        Tank tank = new Tank(300,30,50,50,30);
         FirstAid firstAid = new FirstAid(150,30,50,50);
         new Barrier(50,50,50,50);
         new Barrier(50,175,50,50);
         new Scoring(50,5,30,30);
-        new BladeSwitch(400,70,70,50);
+        new BladeSwitch(400,70,70,40);
         DownCounter timer = new DownCounter(5,5,30,30);
         //创建控制面板对象，并使之关联tank对象
         KeyControlPanel keyControlPanel = new KeyControlPanel(tank);
@@ -157,5 +195,6 @@ public class BladeSwitch {
         Commons.executorService.execute(firstAid);
         Commons.executorService.execute(timer);
         Commons.executorService.execute(tank);
+
     }
 }
